0096c699f35cae33823461ce7234d19a054a02ca
Mathilde <mat.Grimal@gmail.com>
2021-03-17 22:57:48 +0100

    envoi de Test.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Header from './Header';
+import Test from './Test';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Test />
     </div>
   );
 }
---
+++ src/Test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo192.png';
+
+const Test = () => {
+    return (
+        <div>
+            <img src={logo} alt='logo'/>
+        </div>
+    )
+}
+
+export default Test;
